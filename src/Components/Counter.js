5e235303544ed9ba1e9fdbb491a037565099ca39
AgneVasiu <agne.vasiukeviciute@stud.viko.lt>
2022-08-14 16:10:02 +0300

    first commit

---
+++ src/Components/Counter.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import "../Counter.css";
+
+
+function Counter(){
+    let count = 0;
+    const[currentstate, updatedstate] = useState(count);
+    const handleclick = () =>{
+        updatedstate(currentstate + 1);
+    };
+    return(
+        <>
+            <div className="counter-parent">
+                <div className="main-div">
+                    <h3>Counter</h3>
+                    <button onClick={handleclick}>{currentstate}</button>
+                </div>
+            </div>
+        </>
+       
+    );
+
+}
+export default Counter;
